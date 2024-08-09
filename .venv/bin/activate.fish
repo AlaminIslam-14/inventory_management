6354bdade151c5db99a6e5d32455bd98db624f09
Md. Fahad Mir <md.fahadmir@Fahad.local>
2024-08-09 12:23:08 +0600

    created order app

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/alamin/Desktop/Inventory-Management/.venv"
+set -gx VIRTUAL_ENV "/Users/md.fahadmir/Desktop/clone/inventory_management/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
